015ba55de68ae1d4ac324fc56bfac5ff85337805
Irtaza Naqvi <irtaza@mira-ig.de>
2023-06-14 22:31:17 +0500

    Updates

---
+++ precache-manifest.91d202b47387ab389f03a066e5f61dcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fddb951d9e39918b26a51db628c58c6",
+    "url": "/irtaza1234/bleota2/index.html"
+  },
+  {
+    "revision": "4d3427e3cbb50de12502",
+    "url": "/irtaza1234/bleota2/static/css/main.7e6de23a.chunk.css"
+  },
+  {
+    "revision": "63434a638cabb531d86c",
+    "url": "/irtaza1234/bleota2/static/js/2.ddd56c7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/irtaza1234/bleota2/static/js/2.ddd56c7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d3427e3cbb50de12502",
+    "url": "/irtaza1234/bleota2/static/js/main.8038906f.chunk.js"
+  },
+  {
+    "revision": "431596eafb7ddbd8b7fd",
+    "url": "/irtaza1234/bleota2/static/js/runtime-main.49262ad9.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/irtaza1234/bleota2/precache-manifest.91d202b47387ab389f03a066e5f61dcf.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/irtaza1234/bleota2/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
